3e8b1910303b636fd18357ac662b4fb4b2b1675e
saroj bata <lordhunter017@gmail.com>
2023-11-28 07:33:27 +0545

    js files added

---
+++ Mega_JS/veriables.js
@@ -0,0 +1,14 @@
+//veriables - They are very important concept in every programing language that are used to hold (store) and mainpulate them.
+// veriables can store values and expressions.
+// Expressions are something that will eventually resolves to a value.
+// Variables hold the result of an expression, not the expression itself.
+
+
+// js is Dynamically Typed Programming Language. 
+// syntax:
+// variable_type variable_name = value or expression    
+
+const a = 5;
+var b = 10;
+let c = 4;
+console.log(a, b, c);
